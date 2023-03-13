695c213e6b5778be5c2c9119d0ce65426b97b66e
Mỹ Nguyễn <mynguyen@Mys-MacBook-Pro.local>
2023-03-12 20:29:31 -0700

    added @HiltViewModel to TasksViewModel

--- app/src/main/java/com/codinginflow/mvvmtodo/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/TasksViewModel.kt
@@ -1,7 +1,9 @@
 package com.codinginflow.mvvmtodo
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class TasksViewModel @Inject constructor(val dao: TaskDao): ViewModel() {
 }
